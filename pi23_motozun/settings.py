6af90781a5e0be65ef045b135f85347e539fb777
sandriel <sandriel.soares@escolar.ifrn.edu.br>
2023-10-26 10:32:01 -0300

    Criação da Views e Urls

--- pi23_motozun/settings.py
+++ pi23_motozun/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pi23_motozun.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
