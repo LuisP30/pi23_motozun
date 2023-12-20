7c231ace53380605a310c0f1752522d4950a3f12
LuisP30 <pimenta.lh2@gmail.com>
2023-12-20 09:48:26 -0300

    criação de models

---
+++ authenticate/custom_manager.py
@@ -0,0 +1,20 @@
+# Criei este arquivo para definir algumas coisas padrão do Django e deixar o arquivo de models mais limpo.
+# A ideia é não precisar utilizar o campo 'nome de usuário' para os usuários do sistema.
+# Este arquivo está sendo importado para os models.
+
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
